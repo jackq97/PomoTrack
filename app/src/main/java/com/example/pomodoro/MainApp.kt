bf01a601bb2cff786d8cada9a1e10aead645442f
jassi <jaskq@proton.me>
2023-06-01 21:17:18 +0530

    added lottie icons

--- app/src/main/java/com/example/pomodoro/MainApp.kt
+++ app/src/main/java/com/example/pomodoro/MainApp.kt
@@ -1,11 +1,9 @@
 package com.example.pomodoro
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,13 +12,23 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieClipSpec
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.pomodoro.navigation.MyNavigation
 import com.example.pomodoro.navigation.NavigationRoutes
 import com.example.pomodoro.ui.theme.AppTheme
@@ -32,7 +40,6 @@ import com.example.pomodoro.util.rememberSnackbarDemoAppState
 @Composable
 fun MainApp(){
 
-
     val appState: SnackbarDemoAppState = rememberSnackbarDemoAppState()
 
     val navBackStackEntry by appState.navController.currentBackStackEntryAsState()
@@ -42,23 +49,38 @@ fun MainApp(){
 
     val inScreenState = rememberSaveable { (mutableStateOf(false)) }
 
+    var checked by remember { mutableStateOf(false) }
+    var isPlaying by remember { mutableStateOf(false) }
+
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.drawer_close)
+    )
+
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        restartOnPlay = false,
+        isPlaying = isPlaying ,
+        speed = if (checked) -1f else 1f,
+        clipSpec = LottieClipSpec.Progress(0f, 1f)
+    )
+
     when (navBackStackEntry?.destination?.route) {
 
         "pomodoro_screen" -> {
-            leftImageVector = Icons.Default.Settings
+            leftImageVector = ImageVector.vectorResource(id = R.drawable.menu)
             rightImageVector = ImageVector.vectorResource(id = R.drawable.pie_chart)
             inScreenState.value = false
         }
 
         "info_screen" -> {
-            leftImageVector = Icons.Default.ArrowBack
+            leftImageVector = ImageVector.vectorResource(id = R.drawable.arrow_back)
             rightImageVector = null
             inScreenState.value = true
         }
 
         "settings_screen" -> {
-            leftImageVector = null
-            rightImageVector = Icons.Default.ArrowForward
+            leftImageVector = ImageVector.vectorResource(id = R.drawable.arrow_back)
+            rightImageVector = null
             inScreenState.value = true
         }
     }
@@ -75,25 +97,25 @@ fun MainApp(){
                         style = MaterialTheme.typography.headlineMedium
                     ) },
                     navigationIcon = {
-                        if (leftImageVector != null) {
-                            IconButton(onClick = {
-                                if (!inScreenState.value) {
-                                    appState.navController.navigate(NavigationRoutes.SettingsScreen.route)
-                                } else {
-                                    appState.navController.popBackStack()
-                                }
-                            }) {
 
-                                Icon(
-                                    imageVector = leftImageVector,
-                                    contentDescription = "settings",
-                                    tint = MaterialTheme.colorScheme.onSurface
-                                )
+                        LottieAnimation(
+                            modifier = Modifier
+                                .size(45.dp)
+                                .clickable { isPlaying = true },
+                            composition = composition,
+                            progress = { progress }
+                        )
 
+                        LaunchedEffect(progress) {
+                            if (isPlaying &&
+                                (progress == 1.0f || progress == 0.0f)
+                            ){
+                                isPlaying = false
+                                checked = !checked
                             }
                         }
-                    },
 
+                                     },
                     actions = {
                         if (rightImageVector != null) {
                             IconButton(onClick = {
---
+++ app/src/main/java/com/example/pomodoro/ui/composables/AnimatedIconButton.kt
@@ -0,0 +1,10 @@
+package com.example.pomodoro.ui.composables
+
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+
